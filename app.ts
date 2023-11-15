99008915a87f2f82f3f274e3c9608cfcdc17cfb0
Ming-der Wang <mingderwang@gmail.com>
2023-11-15 20:19:10 +0800

    rc1 ok for docker

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-const PORT: number = +(process.env.PORT || 8081);
+const PORT: number = +(process.env.PORT || 3000);
 const NODE_ENV = process.env.NODE_ENV ?? "development";
 
 const server = Bun.serve({
